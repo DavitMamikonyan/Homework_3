b7dc1c6c8af9971596b05f4952df8d5152a2e657
DavitMamikonyan <davitimamikonyan@gmail.com>
2024-10-21 22:23:03 +0400

    Add files via upload

---
+++ Homework_5.3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main(){
+ 
+ int arr[] = {1,2,3,4};
+ int n = sizeof(arr) / sizeof(arr[0]);
+ int *parr = arr;
+
+ // Reversation
+
+ for (int i = n-1; i >= 0; i--){
+    printf("%d", *(parr + i));
+ }
+
+printf("\n");
+
+ return 0;
+}
